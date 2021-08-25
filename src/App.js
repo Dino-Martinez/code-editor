8d80086c50b82445e41f70cbc51a582bd18e8dd3
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-08-24 17:25:55 -0700

    Added a language selector

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import './App.css'
-import React from 'react';
-import { useState } from 'react';
-import CodeMirror from '@uiw/react-codemirror';
-import 'codemirror/addon/display/autorefresh';
-import 'codemirror/addon/comment/comment';
-import 'codemirror/addon/edit/matchbrackets';
-import 'codemirror/keymap/sublime';
+import React, { useState } from 'react'
+
+import CodeMirror from '@uiw/react-codemirror'
+import 'codemirror/addon/display/autorefresh'
+import 'codemirror/addon/comment/comment'
+import 'codemirror/addon/edit/matchbrackets'
+import 'codemirror/keymap/sublime'
 
 function App () {
   const [code, setCode] = useState('')
+  const [language, setLanguage] = useState('js')
 
   return (
-    <div className='editor'>
-      <CodeMirror
-        value={code}
-        onChange={(editor, data) => {
-          setCode(editor.getValue())
-        }}
-        height='100%'
-        width='100%'
-        options={{
-          theme: 'dark',
-          keyMap: 'sublime',
-          mode: 'jsx',
-        }}
-      />
+    <div className='container'>
+      <select onChange={(e) => { setLanguage(e.target.value) }}>
+        <option value='js'>JavaScript</option>
+        <option value='python'>Python</option>
+        <option value='c#'>C#</option>
+        <option value='c'>C</option>
+        <option value='c++'>C++</option>
+        <option value='jsx'>JSX</option>
+        <option value='java'>Java</option>
+      </select>
+
+      <div className='editor'>
+        <CodeMirror
+          value={code}
+          onChange={(editor, data) => {
+            setCode(editor.getValue())
+          }}
+          height='100%'
+          width='100%'
+          options={{
+            theme: 'dark',
+            keyMap: 'sublime',
+            mode: language
+          }}
+        />
+      </div>
     </div>
   )
 }
