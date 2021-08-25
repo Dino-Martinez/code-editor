9e2bdacc32c9a7447c7521219d9603f2755d13d3
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-08-24 17:15:47 -0700

    Added CodeMirror component for text editing, syntax highlighting, etc.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react';
+import { useState } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/addon/display/autorefresh';
+import 'codemirror/addon/comment/comment';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/keymap/sublime';
 
-function App() {
+function App () {
+  const [code, setCode] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='editor'>
+      <CodeMirror
+        value={code}
+        onChange={(editor, data) => {
+          setCode(editor.getValue())
+        }}
+        height='100%'
+        width='100%'
+        options={{
+          theme: 'dark',
+          keyMap: 'sublime',
+          mode: 'jsx',
+        }}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
